793c07c2585a441ab1c6b57bed1073d7ecb1e2cd
WebJubayergit config --global user.email <you@example.com>
2022-06-30 17:19:15 +0600

    footer-section done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <h4>footer vai-boin here</h4>
-            
+        <footer className='footer-body'>
+            <div className='footer-description d-flex justify-content-center align-items-center'>
+                <p>Footer Vai Boin here <br></br> <span className='copyright'>Copyright Ⓒ2022 </span></p>
+            </div>
+            <div className='footer-icon d-flex justify-content-center align-items-center'>
+                <i className='icon fa-brands fa-facebook'></i>
+                <i className='icon fa-brands fa-instagram'></i>
+                <i className='icon fa-brands fa-twitter'></i>
+                <i className='icon fa-brands fa-google-plus'></i>
+                <i className='icon fa-brands fa-linkedin'></i>
+
+            </div>
         </footer>
+
+
+
+
+
+        // <footer className='footer-body '>
+        //     <div className='icons mt-5'>
+        //         <i className='icon fa-brands fa-facebook'></i>
+        //         <i className='icon fa-brands fa-instagram'></i>
+        //         <i className='icon fa-brands fa-twitter'></i>
+        //         <i className='icon fa-brands fa-google-plus'></i>
+        //         <i className='icon fa-brands fa-linkedin'></i>
+        //     </div>
+        // </footer>
     );
 };
 
